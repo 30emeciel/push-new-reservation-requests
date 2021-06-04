c196f8d84dd16010ef5891e2bf757ac31e0dce58
Tony <tony.lbvre@gmail.com>
2021-06-04 10:42:48 +0200

    feat: isolate unit tests

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
 
 from box import Box
-from core.firestore_client import db
-from core.slack_message import send_slack_message
+from core import firestore_client
+from core.slack_message import SlackSender
 from core.tpl import render
 
 log = logging.getLogger(__name__)
+
+db = firestore_client.db()
+slack_sender = SlackSender()
 
 
 def from_firestore(event, context):
@@ -42,6 +45,6 @@ def push_new_reservation_request_to_slack(doc_path, event):
     }
     txt = render("new_reservation_fr.txt", data)
 
-    send_slack_message(txt)
+    slack_sender.send_slack_message(txt)
 
 
--- test_main.py
+++ test_main.py
@@ -1,20 +1,56 @@
-from unittest import TestCase
+from datetime import datetime, timezone, timedelta
 
 import dotenv
+import pytest
 from box import Box
+from core import firestore_client
+from mockito import mock
+from google.cloud.firestore_v1 import Client, CollectionReference, DocumentSnapshot, Transaction, DocumentReference
+
+PARIS_TZ = timezone(timedelta(hours=2))
 
 
-class Test(TestCase):
+@pytest.fixture(autouse=True)
+def setup():
+    dotenv.load_dotenv()
 
-    def setUp(self) -> None:
-        dotenv.load_dotenv()
 
-    def test_push_new_reservation_request_to_slack(self):
-        import main
-        doc_path = 'pax/auth0|5ff87d92a54dd0006f957407/requests/MPWWj2CgAORm3AQS7Vj7'
-        event = {
-            "state": "PENDING_REVIEW"
-        }
+@pytest.fixture(autouse=False)
+def db(when):
+    ret = mock(Client)
+    when(firestore_client).db().thenReturn(ret)
+    return ret
 
-        main.push_new_reservation_request_to_slack(doc_path=doc_path, event=Box(event))
+
+def test_push_new_reservation_request_to_slack(when, db):
+
+
+    import main
+
+    doc_path = 'pax/auth0|5ff87d92a54dd0006f957407/requests/MPWWj2CgAORm3AQS7Vj7'
+
+    pax_ref_mock = mock(spec=DocumentReference)
+    pax_doc_mock = mock({"exists": True}, spec=DocumentSnapshot)
+    pax_data_mock = {
+        "name": "Name",
+    }
+    reservation_ref_mock = mock({"parent": Box({"parent": pax_ref_mock})}, spec=DocumentReference)
+    reservation_doc_mock = mock({"exists": True}, spec=DocumentSnapshot)
+    reservation_data_mock = {
+        "state": "PENDING_REVIEW",
+        "kind": "COLIVING",
+        "arrival_date": datetime(2022, 2, 20, tzinfo=PARIS_TZ),
+        "departure_date": datetime(2022, 2, 26, tzinfo=PARIS_TZ),
+        "number_of_nights": 7,
+    }
+    when(db).document(doc_path).thenReturn(reservation_ref_mock)
+    when(pax_ref_mock).get().thenReturn(pax_doc_mock)
+    when(pax_doc_mock).to_dict().thenReturn(pax_data_mock)
+    when(reservation_ref_mock).get().thenReturn(reservation_doc_mock)
+    when(reservation_doc_mock).to_dict().thenReturn(reservation_data_mock)
+    event = {
+        "state": "PENDING_REVIEW"
+    }
+
+    main.push_new_reservation_request_to_slack(doc_path=doc_path, event=Box(event))
 
